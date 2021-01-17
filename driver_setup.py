1d3db0ac223c1cbad031cabe88f3bf76f261aba3
Grzegorz Pachota <grzegorzpachota@MacBook-Pro-Grzegorz.local>
2021-01-17 15:50:20 +0100

    Changed Chrome to headless

--- driver_setup.py
+++ driver_setup.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 def start_chrome(base_url, exec_path):
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(exec_path, options=chrome_options)
     driver.get(base_url)
     return driver
