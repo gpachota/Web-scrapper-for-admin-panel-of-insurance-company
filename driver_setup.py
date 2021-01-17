361475994cf080573b542ba44170e7dc99d1e26c
Grzegorz Pachota <grzegorzpachota@MacBook-Pro-Grzegorz.local>
2021-01-17 15:42:47 +0100

    Changed driver setup

---
+++ driver_setup.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+
+def start_chrome(base_url, exec_path):
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(exec_path, options=chrome_options)
+    driver.get(base_url)
+    return driver
+
+
+if __name__ == "__main__":
+    start_chrome()
